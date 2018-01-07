2887f0c3c87446e62385d06cc14a318985d4c1c8
yasuhisa <yt>
2018-01-07 06:40:04 +0000

    post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   before_save { self.email.downcase! }
   validates :name, presence:true, length: { maximum: 50 }
-  has_secure_password #パスワード付きのモデル作成
+
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
+  has_secure_password #パスワード付きのモデル作成
+  
+  has_many :microposts
 end
